a7b86ce061dfa560bfc03bc765a9f5f050ef2c1c
keeperlee <leejamesjian@163.com>
2018-10-15 12:00:33 +0800

    日志处理实现-查看

--- sm_web/src/main/java/com/jerry/global/LogAdvice.java
+++ sm_web/src/main/java/com/jerry/global/LogAdvice.java
@@ -4,10 +4,7 @@ import com.jerry.entity.Log;
 import com.jerry.entity.Staff;
 import com.jerry.service.LogService;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,62 +14,57 @@ import javax.servlet.http.HttpSession;
 @Component
 @Aspect
 public class LogAdvice {
-
     @Autowired
     private LogService logService;
-
-    @After("execution(* com.jerry.controller.*.*(..)) && !execution(* com.jerry.controller.SelfController.*(..)) && !execution(* com.jerry.controller.*.to*(..))")
+    @AfterReturning("execution(* com.jerry.controller.*.*(..)) && !execution(* com.jerry.controller.SelfController.*(..)) && !execution(* com.jerry.controller.*.to*(..))")
     public void operationLog(JoinPoint joinPoint){
         Log log = new Log();
         log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
         log.setOperation(joinPoint.getSignature().getName());
-        HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+        HttpServletRequest request =(HttpServletRequest) joinPoint.getArgs()[0];
         HttpSession session = request.getSession();
         Object obj = session.getAttribute("USER");
-        Staff staff = (Staff)obj;
+        Staff staff =(Staff)obj;
         log.setOperator(staff.getAccount());
         log.setResult("成功");
         logService.addOperationLog(log);
     }
-
-    @AfterThrowing(throwing = "e",pointcut = "execution(* com.jerry.controller.*.*(..)) && !execution(* com.jerry.controller.SelfController.*(..))")
+    @AfterThrowing(throwing ="e",pointcut ="execution(* com.jerry.controller.*.*(..)) && !execution(* com.jerry.controller.SelfController.*(..))")
     public void systemLog(JoinPoint joinPoint,Throwable e){
         Log log = new Log();
         log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
         log.setOperation(joinPoint.getSignature().getName());
-        HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+        HttpServletRequest request =(HttpServletRequest) joinPoint.getArgs()[0];
         HttpSession session = request.getSession();
         Object obj = session.getAttribute("USER");
-        Staff staff = (Staff)obj;
+        Staff staff =(Staff)obj;
         log.setOperator(staff.getAccount());
         log.setResult(e.getClass().getSimpleName());
         logService.addSystemLog(log);
     }
-
-   @After("execution(* com.jerry.controller.SelfController.login(..))")
+    @After("execution(* com.jerry.controller.SelfController.login(..))")
     public void loginLog(JoinPoint joinPoint){
-      log(joinPoint);
+        log(joinPoint);
     }
-
     @Before("execution(* com.jerry.controller.SelfController.logout(..))")
     public void logoutLog(JoinPoint joinPoint){
-       log(joinPoint);
+        log(joinPoint);
     }
-
     private void log(JoinPoint joinPoint){
         Log log = new Log();
         log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
         log.setOperation(joinPoint.getSignature().getName());
-        HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+        HttpServletRequest request =(HttpServletRequest) joinPoint.getArgs()[0];
         HttpSession session = request.getSession();
         Object obj = session.getAttribute("USER");
-        if(obj == null){
+        if(obj==null){
             log.setOperator(request.getParameter("account"));
             log.setResult("失败");
-        }else{
-            Staff staff = (Staff)obj;
+        }else {
+            Staff staff = (Staff) obj;
             log.setOperator(staff.getAccount());
             log.setResult("成功");
-        } logService.addSystemLog(log);
+        }
+        logService.addLoginLog(log);
     }
 }
